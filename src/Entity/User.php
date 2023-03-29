1efd7657bfaeae9a17f061a6130c639ee8a59f6a
juansdev <juansdev72@gmail.com>
2023-03-29 20:22:41 +0000

    FEAT: Agregar Dockerfile y docker-compose     - Agregar la entidad y migración de User     - Configurar .env     - Agregar .idea

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,11 +10,91 @@ class User
 {
     #[ORM\Id]
     #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
+    #[ORM\Column(type: 'integer', nullable: false)]
+    private int $idUser;
 
-    public function getId(): ?int
+    #[ORM\Column(type: 'string', length: 120, nullable: false)]
+    private string $name;
+
+    #[ORM\Column(type: 'string', length: 120, nullable: false)]
+    private string $lastname;
+
+    #[ORM\Column(type: 'string', length: 120, nullable: false)]
+    private string $email;
+
+    #[ORM\Column(type: 'string', length: 120, nullable: false)]
+    private string $password;
+
+    #[ORM\Column(type: 'integer', nullable: false)]
+    private int $status;
+
+    public function getId(): int
     {
-        return $this->id;
+        return $this->idUser;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function getLastName(): string
+    {
+        return $this->lastname;
+    }
+
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function getStatus(): int
+    {
+        return $this->status;
+    }
+
+    public function getIdUser(): ?int
+    {
+        return $this->idUser;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function setLastname(string $lastname): self
+    {
+        $this->lastname = $lastname;
+
+        return $this;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    public function setStatus(int $status): self
+    {
+        $this->status = $status;
+
+        return $this;
     }
 }
