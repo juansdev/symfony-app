eb860ae052c55debdd48444fa6096213f1ef943c
juansdev <juansdev72@gmail.com>
2023-04-04 23:11:55 +0000

    FEAT: Agregar vista User

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -11,9 +11,6 @@ class UserController extends AbstractController
     #[Route('/user', name: 'app_user')]
     public function index(): JsonResponse
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/UserController.php',
-        ]);
+        return $this->render('user/users.html.twig');
     }
 }
